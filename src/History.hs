3a0ddb996ae20377538069cd9dfaf9d09942a10d
Leonard Ganz <leonard@ruediger-ganz.de>
2022-03-16 15:49:40 +0100

    Support extensions of histories via QQ

--- src/History.hs
+++ src/History.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE DeriveLift #-}
 
 module History
   ( History (..),
@@ -7,36 +8,55 @@ module History
     DistrictInfo (..),
     Snapshot (..),
     parseHistory,
+    parseBodyPlain,
     parseHistoryPlain,
     removeBlanks,
+    appendBody,
   )
 where
 
 import Data.Char (isSpace)
 import Data.Data
 import Data.Time.Calendar (Day, fromGregorian)
+import Language.Haskell.TH.Syntax hiding (Body)
 import Text.ParserCombinators.Parsec
 import Text.ParserCombinators.Parsec.Number
 
-data History = History Header Body | AntiHistory String
+data History
+  = History Header Body
+  | AntiHistory String -- history from string var
+  | ExtendedHistory String Body -- history supplied by history var via $hist: , body parsed normally
+  | AntiExtendedHistory String String -- history supplied by history var via $hist: , body parsed from string var
   deriving (Show, Typeable, Data)
 
 newtype Header = Header [DistrictInfo]
   deriving (Show, Typeable, Data)
 
 newtype Body = Body [Snapshot]
-  deriving (Show, Typeable, Data)
+  deriving (Show, Typeable, Data, Lift)
 
 data DistrictInfo = DistrictInfo {ags :: String, name :: String, stateAbbreviation :: String, population :: Int}
   deriving (Show, Typeable, Data)
 
 data Snapshot = Snapshot Day [Double]
-  deriving (Show, Typeable, Data)
+  deriving (Show, Typeable, Data, Lift)
+
+instance Lift Day where
+  lift = dataToExpQ (const Nothing)
+  liftTyped = unsafeTExpCoerce . lift
+
+appendBody :: History -> Body -> History
+appendBody (History h (Body b)) (Body b') = History h $ Body (b ++ b')
+appendBody _ _ = error "invalid call. appendBody only supports default histories"
 
 -- for how to use parsec http://book.realworldhaskell.org/read/using-parsec.html was used among other resources
 followArrow = string "=>"
 
-history = (History <$> header <*> body) <|> antiHistory
+history =
+  try (History <$> header <*> body)
+    <|> try antiExtendedHistory
+    <|> try extendedHistory
+    <|> antiHistory
 
 textCell = many (noneOf ",\\\n\r")
 
@@ -95,22 +115,31 @@ eol =
     <|> string "\r"
     <?> "end of line"
 
--- code for anti quotes, small, large, idchar & ident adapted from guide https://www.schoolofhaskell.com/user/marcin/quasiquotation-101
-antiHistory = char '$' >> AntiHistory <$> ident
+antiHistory = AntiHistory <$> stringVar
+
+extendedHistory = ExtendedHistory <$> histVar <* possiblyEscapedEol <*> body
+
+antiExtendedHistory = AntiExtendedHistory <$> histVar <* optional possiblyEscapedEol <*> stringVar
+
+histVar = string "$hist:" *> ident <?> "history variable"
+
+stringVar = char '$' *> ident <?> "string variable"
+
+-- code for small, large, idchar & ident adapted from guide https://www.schoolofhaskell.com/user/marcin/quasiquotation-101 and https://wiki.haskell.org/Quasiquotation
 
 small = lower <|> char '_'
 
 large = upper
 
-idchar = small <|> large <|> digit <|> char '\''
+idchar = small <|> large <|> digit <|> char '\'' <?> "char of identifier"
 
-ident = do
-  c <- small
-  cs <- many idchar
-  return (c : cs)
+ident = (:) <$> small <*> many idchar <?> "identifier for variable"
 
 parseHistoryPlain :: String -> Either ParseError History
 parseHistoryPlain = parse history "(unknown)" . removeBlanks
+
+parseBodyPlain :: String -> Either ParseError Body
+parseBodyPlain = parse body "(unknown)" . removeBlanks
 
 -- adapted from wiki QQ Expr example https://wiki.haskell.org/Quasiquotation
 parseHistory :: Monad m => (String, Int, Int) -> String -> m History
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,31 +1,20 @@
 {-# LANGUAGE QuasiQuotes #-}
 
 module Lib
-  ( someFunc,
-    historyFromFile,
+  ( historyFromFile,
     historyFromDefaultFile,
-    fetchAndSaveData
+    fetchAndSaveData,
+    appendHistoryWithRawBodyData,
+    stringToHistory,
   )
 where
 
+import DataRetrieval.ApiDataManager
 import History
 import HistoryQuote
-import DataRetrieval.ApiDataManager
 
-someFunc :: IO ()
-someFunc = print testQQ
-
-testQQ :: History
-testQQ =
-  [history|
-ags               => 09162, otherAgs
-name              => d1, d2
-stateAbbreviation => BY, BY
-population        => 1234, 23455
-
-2020-12-02        => 123, 444.9
-2012-12-12        => 12, 13.23
-|]
+appendHistoryWithRawBodyData :: History -> String -> History
+appendHistoryWithRawBodyData x s = [history|$hist:x $s|]
 
 stringToHistory :: String -> History
 stringToHistory s = [history|$s|]
@@ -39,3 +28,20 @@ historyFromDefaultFile = historyFromFile "incidence.history"
 
 fetchAndSaveData :: IO ()
 fetchAndSaveData = updateHistoryIncidenceFile
+
+-- for testing:
+
+testQQ :: History
+testQQ =
+  [history|
+ags               => 09162, otherAgs
+name              => d1, d2
+stateAbbreviation => BY, BY
+population        => 1234, 23455
+
+2020-12-02        => 123, 444.9
+2012-12-12        => 12, 13.23
+|]
+
+testExtQQ :: History -> History
+testExtQQ x = [history|$hist:x\n2088-12-02        => 3453, 44554.9\n2012-12-18        => 1552, 13.2355\n|]
