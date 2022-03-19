defec5c39a3e5f4f059eb3ef9665bfccef772d2c
Leonard Ganz <leonard@ruediger-ganz.de>
2022-03-19 13:23:34 +0100

    Create Leneses for hierarchical data structure

--- src/DataStructure.hs
+++ src/DataStructure.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
 
 module DataStructure where
 
@@ -12,33 +13,11 @@ import Data.Text (Text, pack)
 import Data.Time.Calendar (Day)
 import DataRetrieval.ApiDataManager
 import DataRetrieval.ApiDistrict (ags, name, population)
-import Debug.Trace (trace)
 import History
+import Lens.Micro.Platform
 import Util
 
-type CountryKey = Text -- abbreviations like DE
-
-type StateKey = Text -- abbreviations like BY
-
 type DistrictKey = Text -- AGS (allgemeiner Gemeindeschlüssel), e.g. 09162 for Munich (text instead of ints because of leading zeros)
-
-type States = Map StateKey State
-
-type Districts = Map DistrictKey District
-
-data Country = Country
-  { _countryName :: Text,
-    _countryKey :: CountryKey,
-    _states :: States
-  }
-  deriving (Show)
-
-data State = State
-  { _stateName :: Text,
-    _stateKey :: StateKey,
-    _districts :: Districts
-  }
-  deriving (Show)
 
 data District = District
   { _districtName :: Text,
@@ -47,6 +26,34 @@ data District = District
     _weekIncidence :: Double
   }
   deriving (Show)
+
+type Districts = Map DistrictKey District
+
+makeLenses ''District
+
+type StateKey = Text -- abbreviations like BY
+
+data State = State
+  { _stateName :: Text,
+    _stateKey :: StateKey,
+    _districts :: Districts
+  }
+  deriving (Show)
+
+type States = Map StateKey State
+
+makeLenses ''State
+
+type CountryKey = Text -- abbreviations like DE
+
+data Country = Country
+  { _countryName :: Text,
+    _countryKey :: CountryKey,
+    _states :: States
+  }
+  deriving (Show)
+
+makeLenses ''Country
 
 -- result is IO as some information is fetched over http.
 -- result is Nothing if something went wrong with the http request or parsing or if no information was found for the specified district key.
